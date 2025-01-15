c29fd8cad3ec3adeb66b7245285a2a344df044b7
Alan Tavares Morais <pollargithub@gmail.com>
2025-01-15 13:59:58 -0300

    finalização de página de confirmação

--- src/app/(register)/register/(steps)/confirm/page.tsx
+++ src/app/(register)/register/(steps)/confirm/page.tsx
@@ -4,6 +4,9 @@ import { type RootState, type AppDispatch } from "@/stores/store";
 import CheckRegister from "@/components/CheckRegister";
 import { updateState } from "@/stores/features/storeRegister";
 import BulletCheck from "@/components/icons/BulletCheck";
+import Link from "next/link";
+import Image from "next/image";
+import IconLink from "@/components/icons/IconLink";
 
 export default function ConfirmRegisterPage(){
     const state = useSelector((rootState:RootState) => rootState.register);
@@ -28,10 +31,16 @@ export default function ConfirmRegisterPage(){
             </div>
 
             <div className="px-2">
-                <button onClick={confirmTerms} className="font-bold flex items-center gap-1">
+                <button onClick={confirmTerms} className="font-bold flex items-center gap-3 mb-2">
                     <BulletCheck check={ state.terms }/>
                     <span className="block">Eu aceito os Termos de serviço</span>
                 </button>
+                <div className="px-8">
+                    <Link href="#" className="text-mini flex items-center gap-1 hover:underline">
+                        &gt; Ler Termos
+                        <IconLink/>
+                    </Link>
+                </div>
             </div>
         </div>
     )
